6f869d24baf20564bafb6f7619884f0b7e028927
Saumya <saumya.20223238@mnnit.ac.in>
2026-02-19 17:25:33 +0530

    Small debugging

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "https://paise-pe-nazar.vercel.app/"
+        "https://paise-pe-nazar.vercel.app"
     ],
     credentials: true
 }));
